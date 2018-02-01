445cd7c91322d036c572e80e4811b58e57c611f8
pkaijun <kai_jun96@hotmail.com>
2018-02-02 00:25:26 +0800

    Learning Outcomes - 1KLoC (Code Enhancement [Case Insensitive for 'find'])

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -561,7 +561,7 @@ public class AddressBook {
      * @return set of keywords as specified by args
      */
     private static Set<String> extractKeywordsFromFindPersonArgs(String findPersonCommandArgs) {
-        return new HashSet<>(splitByWhitespace(findPersonCommandArgs.trim()));
+        return new HashSet<>(splitByWhitespace(findPersonCommandArgs.trim().toLowerCase()));
     }
 
     /**
@@ -573,7 +573,7 @@ public class AddressBook {
     private static ArrayList<HashMap<Integer, String>> getPersonsWithNameContainingAnyKeyword(Collection<String> keywords) {
         final ArrayList<HashMap<Integer, String>> matchedPersons = new ArrayList<>();
         for (HashMap<Integer, String> person : getAllPersonsInAddressBook()) {
-            final Set<String> wordsInName = new HashSet<>(splitByWhitespace(getNameFromPerson(person)));
+            final Set<String> wordsInName = new HashSet<>(splitByWhitespace(getNameFromPerson(person).toLowerCase()));
             if (!Collections.disjoint(wordsInName, keywords)) {
                 matchedPersons.add(person);
             }
